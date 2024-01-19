ea33b5e9c74b31a5fcd702f78100f324fc43798b
Project-Faust <pentecost.cody.lane@gmail.com>
2024-01-19 18:36:41 -0500

    created file to store denomination name/value

---
+++ src/utils/currency.js
@@ -0,0 +1,57 @@
+export const notes = [
+    {
+        name: '$100',
+        value: 100,
+    },
+    {
+        name: '$50',
+        value: 50,
+    },
+    {
+        name: '$20',
+        value: 20,
+    },
+    {
+        name: '$10',
+        value: 10,
+    },
+    {
+        name: '$5',
+        value: 5,
+    },
+    {
+        name: '$2',
+        value: 2,
+    },
+    {
+        name: '$1',
+        value: 1,
+    }
+]
+
+export const coins = [
+    {
+        name: 'Dollar',
+        value: 1
+    },
+    {
+        name: 'Half-Dollar',
+        value: 0.5,
+    },
+    {
+        name: 'Quarter',
+        value: 0.25,
+    },
+    {
+        name: 'Dime',
+        value: 0.10,
+    },
+    {
+        name: 'Nickel',
+        value: 0.05,
+    },
+    {
+        name: 'Penny',
+        value: 0.01,
+    }
+]
